36066222eca6410e5a47dd1baea9f5f9afdf88a5
PetyaKatsarova <pskpetya@gmail.com>
2024-06-21 10:44:08 +0200

    added more code

--- src/main/java/business/Order.java
+++ src/main/java/business/Order.java
@@ -1,4 +1,6 @@
-package business;
+// File: src/main/java/com/grocerystore/model/Order.java
+
+package com.grocerystore.model;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,13 +22,22 @@ public class Order {
 
     public String generateReceipt() {
         StringBuilder receipt = new StringBuilder();
+        receipt.append(String.format("%-8s %-10s %-12s %-12s %-10s %-16s\n", "amount", "item", "single price", "price", "discount", "totalAfterDiscount"));
+
         double total = 0.0;
         for (Item item : items) {
-            double itemTotal = item.getTotalPrice();
-            receipt.append(String.format("%s: €%.2f\n", item.getName(), itemTotal));
-            total += itemTotal;
+            double singlePrice = item.getPrice();
+            int amount = 1;
+            if (item instanceof Beer) {
+                amount = ((Beer) item).getQuantity();
+            }
+            double price = singlePrice * amount;
+            double totalAfterDiscount = item.getTotalPrice();
+            double discount = price - totalAfterDiscount;
+            receipt.append(String.format("%-8d %-10s €%-11.2f €%-11.2f €%-9.2f €%-15.2f\n", amount, item.getName(), singlePrice, price, discount, totalAfterDiscount));
+            total += totalAfterDiscount;
         }
-        receipt.append(String.format("Total: €%.2f", total));
+        receipt.append(String.format("\nTotal: €%.2f", total));
         return receipt.toString();
     }
 }
