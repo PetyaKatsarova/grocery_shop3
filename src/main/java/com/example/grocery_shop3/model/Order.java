eec73d3b5d045b42ab156a4976b4bbf6dd85be00
PetyaKatsarova <pskpetya@gmail.com>
2024-06-27 16:26:36 +0200

    most model classes are done

--- src/main/java/com/example/grocery_shop3/model/Order.java
+++ src/main/java/com/example/grocery_shop3/model/Order.java
@@ -16,25 +16,32 @@ public class Order {
         items.add(item);
     }
 
+    /**
+     * Example order w/ prices:
+     * Bread €1,00, Veg €1,00 per 100g, Beer €0,50 per bottle
+     * 1. 3 x Bread (three days old) €2,00
+     * 2. 200g Vegetables €1,86
+     * 3. 6 x Dutch Beers €2,00
+     *    Total: €5,86
+     *    ○ Given an Order, return the total cost with discounts applied,
+     * and a receipt that breaks down the prices
+     *  List the current discount rules
+     * ○ List the current prices per item
+     */
     public double calculateTotal() {
-        return items.stream().mapToDouble(Item::getTotalPrice).sum();
+        return items.stream().mapToDouble(Item::getTotalAfterDiscount).sum();
     }
 
     public String generateReceipt() {
         StringBuilder receipt = new StringBuilder();
-        receipt.append(String.format("%-8s %-10s %-12s %-12s %-10s %-16s\n", "amount", "item", "single price", "price", "discount", "totalAfterDiscount"));
-
+        receipt.append(String.format("%-6s %-10s %-10s %-10s %-10s %-10s\n", "Qty", "Item", "UnitPrice", "Price", "Discount", "Total"));
         double total = 0.0;
         for (Item item : items) {
-            double singlePrice = item.getPrice();
-            int amount = 1;
-            if (item instanceof Beer) {
-                amount = ((Beer) item).getQuantity();
-            }
-            double price = singlePrice * amount;
-            double totalAfterDiscount = item.getTotalPrice();
-            double discount = price - totalAfterDiscount;
-            receipt.append(String.format("%-8d %-10s €%-11.2f €%-11.2f €%-9.2f €%-15.2f\n", amount, item.getName(), singlePrice, price, discount, totalAfterDiscount));
+            int qty = item instanceof Beer ? ((Beer) item).getQuantity() : (item instanceof Bread ? ((Bread) item).getQuantity() : 1);
+            double price = item.getPrice() * qty;
+            double discount = item.getDiscount();
+            double totalAfterDiscount = item.getTotalAfterDiscount();
+            receipt.append(String.format("%-6d %-10s €%-9.2f €%-9.2f €%-9.2f €%-9.2f\n", qty, item.getName(), item.getPrice(), price, discount, totalAfterDiscount));
             total += totalAfterDiscount;
         }
         receipt.append(String.format("\nTotal: €%.2f", total));
